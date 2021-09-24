d4066235f7568cac3f196e38672747a986249034
MichaelWang <michael_wangguanyi@163.com>
2021-09-24 15:41:02 +0800

    receipt in sample

--- sample/main.go
+++ sample/main.go
@@ -1,11 +1,11 @@
 package main
 
 import (
+	"context"
 	"ethcli"
 	"fmt"
 	ethcmn "github.com/ethereum/go-ethereum/common"
 	"log"
-	"math/big"
 )
 
 func main() {
@@ -14,22 +14,28 @@ func main() {
 		log.Fatalln(err)
 	}
 
-	amount, ok := new(big.Int).SetString("1000000000000000000000000000000000", 10)
-	if !ok {
-		panic("convert failed")
-	}
+	//amount, ok := new(big.Int).SetString("1000000000000000000000000000000000", 10)
+	//if !ok {
+	//	panic("convert failed")
+	//}
+	//
+	//fmt.Println(amount)
+	//txHash, err := cli.CallContract(
+	//	"ERC20Token",
+	//	big.NewInt(0),
+	//	"mintDirectly",
+	//	ethcmn.HexToAddress("0xF202E4e0EB3C10c4F0ace15aF6B6EA3AFAe777AC"),
+	//	amount,
+	//)
+	//if err != nil {
+	//	log.Fatalln(err)
+	//}
+	//
+	//fmt.Println(txHash)
 
-	fmt.Println(amount)
-	txHash, err := cli.CallContract(
-		"ERC20Token",
-		big.NewInt(0),
-		"mintDirectly",
-		ethcmn.HexToAddress("0xF202E4e0EB3C10c4F0ace15aF6B6EA3AFAe777AC"),
-		amount,
-	)
+	receipt, err := cli.TransactionReceipt(context.Background(), ethcmn.HexToHash("0x230022E4820DD1F3896BD90D7DD1147433C097AB79EDAE13E3FBA6B51D9F8EF8"))
 	if err != nil {
 		log.Fatalln(err)
 	}
-
-	fmt.Println(txHash)
+	fmt.Println(receipt)
 }
